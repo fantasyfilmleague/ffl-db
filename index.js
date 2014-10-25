fefc3be6041fec16ad92442f96e165e588272ec1
Colin Teal <the30yearswar@hotmail.com>
2014-10-25 16:23:18 -0700

    Add query support

--- index.js
+++ index.js
@@ -2,14 +2,76 @@
 
 var pg = require('pg');
 var fs = require('fs');
+var util = require('util');
+var _ = require('underscore');
+
+exports.createConnectionStringFromEnvironment = function () {
+  var env = process.env;
+  var format = 'postgres://%s:%s@%s/%s';
+  return util.format(format, env.POSTGRES_USERNAME, env.POSTGRES_PASSWORD, env.POSTGRES_HOST, env.POSTGRES_DATABASE);
+};
+
+exports.getConnectionStringFromEnvironment = function () {
+  var env = process.env;
+  var connectionString = env.POSTGRES_CONNECTION_STRING;
+  return (connectionString) ? connectionString : exports.createConnectionStringFromEnvironment();
+};
 
 var connect = exports.connect = function (connectionString, callback) {
-  pg.connect(connectionString, function(error, client, done) {
+  var effectiveConnectionString = connectionString;
+  var effectiveCallback = callback;
+
+  if (arguments.length === 1) {
+    effectiveCallback = connectionString;
+    effectiveConnectionString = exports.getConnectionStringFromEnvironment();
+  }
+
+  pg.connect(effectiveConnectionString, function(error, client, done) {
     if(error) {
+      return effectiveCallback(error);
+    }
+
+    effectiveCallback(null, client, done);
+  });
+};
+
+var query = exports.query = function (sql, params, callback) {
+  var temp;
+  var effectiveSql = sql;
+  var effectiveParams = params;
+  var effectiveCallback = callback;
+
+  if (_.isFunction(sql.toParams)) {
+    temp = sql.toParams();
+    effectiveSql = temp.text;
+    effectiveParams = temp.values;
+    effectiveCallback = (_.isFunction(params)) ? params : callback;
+  }
+
+  connect(function (error, client, done) {
+    if (error) {
+      return effectiveCallback(error);
+    }
+
+    client.query(effectiveSql, effectiveParams, effectiveCallback);
+  });
+};
+
+exports.queryOne = function (sql, params, callback) {
+  var effectiveParams = params;
+  var effectiveCallback = callback;
+
+  if (arguments.length === 2 && _.isFunction(params)) {
+      effectiveCallback = params;
+      effectiveParams = null;
+  }
+
+  query(sql, effectiveParams, function (error, result) {
+    if (error) {
       return callback(error);
     }
 
-    callback(null, client, done);
+    callback(null, _.first(result.rows));
   });
 };
 
