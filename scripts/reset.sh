e28ec58513ec1fcb09ac09fe7e0be46f1738c751
Colin Teal <the30yearswar@hotmail.com>
2014-10-26 01:31:13 -0700

    Add bin, use environment variables

--- scripts/reset.sh
+++ scripts/reset.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-sudo -u postgres psql -f "`dirname $0`/../sql/delete.sql"
-sudo -u postgres psql -f "`dirname $0`/../sql/create.sql"
-sudo -u postgres psql -f "`dirname $0`/../sql/schema.sql" -d fantasy_film_league
+sudo -u $POSTGRES_USERNAME psql -f "`dirname $0`/../sql/delete.sql"
+sudo -u $POSTGRES_USERNAME psql -f "`dirname $0`/../sql/create.sql"
+sudo -u $POSTGRES_USERNAME psql -f "`dirname $0`/../sql/schema.sql" -d $POSTGRES_DATABASE
